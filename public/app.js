920928672d902a59835b06f4c6113fdd8d79b340
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-10-08 21:58:02 -0500

    adding text area for nortes

--- public/app.js
+++ public/app.js
@@ -9,7 +9,7 @@ $.getJSON("/articles", function (data) {
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "header", function () {
+$(document).on("click", ".meta", function () {
     // Empty the notes from the note section
     $("#notes").empty();
     // Save the id from the p tag
@@ -26,9 +26,9 @@ $(document).on("click", "header", function () {
             // The title of the article
             $("#notes").append("<h2>" + data.title + "</h2>");
             // An input to enter a new title
-            $("#notes").append("<input id='titleinput' name='title' >");
+            $("#notes").append("<input class='list-group-item' id='titleinput' name='title' >");
             // A textarea to add a new note body
-            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            $("#notes").append("<textarea class='list-group-item-text' id='bodyinput' name='body'></textarea>");
             // A button to submit a new note, with the id of the article saved to it
             $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
