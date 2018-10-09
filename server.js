60add7015cf7753de7aacf2797916dbe98c85137
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-10-08 20:48:44 -0500

    some of the server is working/getting data back

--- server.js
+++ server.js
@@ -11,6 +11,25 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 //Mongo db needs to be linked here
-mongoose.connect()
+mongoose.connect("mongodb://localhost/scrapin", { useNewUrlParser: true });
 
-//this will be all of my routes for the app on the backend
+//this will be all of my routes for the app on the backend.
+app.get("/scrape", function(req, res){
+    axios.get("https://news.ycombinator.com/").then(function(response){
+        let $ = cheerio.load(response.data);
+
+       $("tr td").each(function(i, element) {
+           let result = {};
+
+           result.title = $(this).children("a").text();
+           console.log(result.title);
+       });
+
+       res.send("Scrape Completed")
+    })
+});
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
